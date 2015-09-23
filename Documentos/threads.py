f3c9abd368cc5663e21b6739258ca00a6d3a4e40
zoraidams <zoraidamss@yahoo.es>
2015-09-23 21:56:40 +0200

    blink.py

---
+++ Documentos/threads.py
@@ -0,0 +1,13 @@
+import threading
+
+def worker(count):
+  """funcion que realiza el trabajo en el thread"""
+  print "Este es el %s trabajo que hago hoy" % count
+  return
+
+threads = list()
+
+for i in range(3):
+  t = threading.Thread(target=worker, args=(i,))
+  threads.append(t)
+  t.start()
