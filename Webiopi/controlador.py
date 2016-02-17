f1b5592d11fadde0506869208af3da0dc58d17a7
root <root@raspberrypi.(none)>
2016-02-17 15:38:36 +0100

    Añadida en el controlador la funcionalidad de capturar imágenes mientras se mueve.

--- Webiopi/controlador.py
+++ Webiopi/controlador.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python
 import webiopi
 import time
-from cv2.cv import *
-##import cv2
+import cv2
 
 # Libreria GPIO
 GPIO = webiopi.GPIO
@@ -10,12 +10,15 @@ GPIO = webiopi.GPIO
 # Definicion constantes                              #
 # -------------------------------------------------- #
 
+# Pines motor derecho
 ENA = 27
 IN1 = 17
 IN2 = 22
+# Pines motor izquierdo
 ENB = 18
 IN3 = 23
 IN4 = 24
+# Pines sensor distancia
 TRIGGER = 5
 ECHO = 6
 
@@ -71,44 +74,10 @@ def distance():
     return distance
 
 def picture():
-    # Initialize the camera
-    capture = CaptureFromCAM(0)  # 0 -> index of camera
-    if capture:     # Camera initialized without any errors
-       f = QueryFrame(capture)     # capture the frame
-       if f:
-           ShowImage("cam-test",f)
-
-##    # Camera 0 is the integrated web cam on my netbook
-##    camera_port = 0
-##    
-##    #Number of frames to throw away while the camera adjusts to light levels
-##    ramp_frames = 30
-## 
-##    # Now we can initialize the camera capture object with the cv2.VideoCapture class.
-##    # All it needs is the index to a camera port.
-##    camera = cv2.VideoCapture(camera_port)
-##    
-##    # Captures a single image from the camera and returns it in PIL format
-##    def get_image():
-##        # read is the easiest way to get a full image out of a VideoCapture object.
-##        retval, im = camera.read()
-##        return im
-##
-##    # Ramp the camera - these frames will be discarded and are only used to allow v4l2
-##    # to adjust light levels, if necessary
-##    for i in xrange(ramp_frames):
-##        temp = get_image()
-##    print("Taking image...")
-##    # Take the actual image we want to keep
-##    camera_capture = get_image()
-##    file = "/home/codeplasma/test_image.png"
-##    # A nice feature of the imwrite method is that it will automatically choose the
-##    # correct format based on the file extension you provide. Convenient!
-##    cv2.imwrite(file, camera_capture)
-##
-##    # You'll want to release the camera, otherwise you won't be able to create a new
-##    # capture object until your script exits
-##    del(camera)
+    pass
+   # cap = cv2.VideoCapture(0)
+   # ret, img = cap.read()
+   # cv2.imwrite('pic.jpg', img)
 
 # -------------------------------------------------- #
 # Definicion macros                                  #
@@ -124,25 +93,25 @@ def go_forward():
 def go_backward():
     backward()
     distance()
-    picture()
+#    picture()
 
 @webiopi.macro
 def turn_left():
     left()
     distance()
-    picture()
+#    picture()
 
 @webiopi.macro
 def turn_right():
     right()
     distance()
-    picture()
+#    picture()
 
 @webiopi.macro
 def stop_motors():
     stop()
     distance()
-    picture()
+#    picture()
     
 # -------------------------------------------------- #
 # Iniciacializacion                                  #
@@ -150,22 +119,28 @@ def stop_motors():
 
 def setup():
     # Instalacion GPIOs
+    # Motor derecho
     #GPIO.setFunction(ENA, GPIO.OUT)
     GPIO.setFunction(IN1, GPIO.OUT)
     GPIO.setFunction(IN2, GPIO.OUT)
+    # Motor izquierdo
     #GPIO.setFunction(ENB, GPIO.OUT)
     GPIO.setFunction(IN3, GPIO.OUT)
     GPIO.setFunction(IN4, GPIO.OUT)
+    # Sensor distancia
     GPIO.setFunction(TRIGGER, GPIO.OUT)
     GPIO.setFunction(ECHO, GPIO.IN)
 
 def destroy():
     # Resetea las funciones GPIO
+    # Motor derecho
     #GPIO.setFunction(ENA, GPIO.IN)
     GPIO.setFunction(IN1, GPIO.IN)
     GPIO.setFunction(IN2, GPIO.IN)
+    # Motor izquierdo
     #GPIO.setFunction(ENB, GPIO.IN)
     GPIO.setFunction(IN3, GPIO.IN)
     GPIO.setFunction(IN4, GPIO.IN)
+    # Sensor distancia
     GPIO.setFunction(TRIGGER, GPIO.IN)
     GPIO.setFunction(ECHO, GPIO.OUT)
