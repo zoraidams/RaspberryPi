fb4dff622a10e42674a7298e4f8524d74657470c
root <root@raspberrypi.(none)>
2016-04-28 17:41:55 +0200

    Deleted a few duplicate files more.

--- OpenCV/procesa.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img = cv2.imread("image.jpg")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-ret, corners = cv2.findChessboardCorners(gray, (5,4), None)
-
-if ret == True:
-  print true
